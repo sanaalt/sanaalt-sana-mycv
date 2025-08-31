c008a29ba489564c46b021612c5dacbaff1b4a45
sana altair <sana7altair@icloud.com>
2025-08-31 20:22:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 $(document).ready(function(){
 
-  $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
-  });
-
-  $(window).on('scroll load',function(){
-
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
-
-  });
 
   $('a[href*="#"]').on('click',function(e){
 
